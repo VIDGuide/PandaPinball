23799035f120457789fe8b5a256897d9c1eec946
Michael Saunders <vidguide@gmail.com>
2013-04-01 17:19:37 +1100

    initial push to GIT

---
+++ MFConsoleApplication1/Pinball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using Microsoft.SPOT;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using GHI.Premium.Hardware;
+
+namespace PinballProject
+{
+
+
+    public class Pinball
+    {
+        public const int PlayerNotActive = 0;
+        public const int PlayerActiveNotPlayer = 1;
+        public const int PlayerActivePlaying = 2;
+        public const int PlayerGameOver = 3;
+
+        public const int Game_GameOver = 0;
+        public const int Game_Standby = 1;
+        public const int Game_InProgress = 2;
+        public const int Game_Ending = 3;
+
+        public static RuleProcessor ScriptEngine = new RuleProcessor();
+
+        public static NightFever Game = new NightFever(); // Change this line to new 'game' class for different playfields/games
+                
+        public static void Main()
+        {
+            Debug.Print("Welcome to " + Pinball.Game.GetGameName() + ". Starting up...");
+            Debug.Print("Game Started. Ready to Run");
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        public void PlaySoundByFileName(string filename)
+        {
+
+        }
+    }
+}
---
+++ MFConsoleApplication1/Switch.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHI.Premium.Hardware;
+
+namespace PinballProject
+{
+    public class Switch
+    {
+        private string SwitchName;
+        private Cpu.Pin IOPort;
+        private string DownScript;
+        private string UpScript;
+
+        public Switch(string switchName, Cpu.Pin IOport, string downScript, string upScript, Port.InterruptMode ActivateState)
+        {
+            SwitchName = switchName;
+            IOPort = IOport;
+            DownScript = downScript;
+            UpScript = upScript;
+
+            InterruptPort IntButton = new InterruptPort(IOPort, false, Port.ResistorMode.PullUp, ActivateState);
+            IntButton.OnInterrupt += new NativeEventHandler(this.Press);
+        }
+
+        public void Press(uint port, uint state, DateTime time)
+        {
+            switch (state)
+            {
+                case 0:
+                    Debug.Print("Switch Pressed: " + SwitchName);
+                    Pinball.ScriptEngine.RunScript(DownScript);
+                    break;
+                case 1:
+                    Debug.Print("Switch Released: " + SwitchName);
+                    Pinball.ScriptEngine.RunScript(UpScript);
+                    break;
+                default:
+                    Debug.Print("Unknown State Detected: " + state.ToString());
+                    break;
+
+            }
+        }
+    }
+}
